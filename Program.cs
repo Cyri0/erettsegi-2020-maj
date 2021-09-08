7f807d122e1ae6f45852299e9b6daddfe747b935
Tanár <nemestamas94@gmail.com>
2021-09-08 10:01:16 +0200

    elso ora vege

--- Program.cs
+++ Program.cs
@@ -26,12 +26,55 @@ namespace playfair_2
             }
         }
 
-        public void showMatix() {
+        // 5. feladat
+        public int sorIndex(char betu) {
             for (int sor = 0; sor < 5; sor++) {
                 for (int oszlop = 0; oszlop < 5; oszlop++) {
-                
+                    if (betu == matrix[sor, oszlop])
+                        return sor;
                 }
             }
+            return -1;
+        }
+
+        // 5. feladat
+        public int oszlopIndex(char betu)
+        {
+            for (int sor = 0; sor < 5; sor++)
+            {
+                for (int oszlop = 0; oszlop < 5; oszlop++)
+                {
+                    if (betu == matrix[sor, oszlop])
+                        return oszlop;
+                }
+            }
+            return -1;
+        }
+
+        public string KodolBetupar(string eredeti) {
+            char kar1 = eredeti[0];
+            char kar2 = eredeti[1];
+
+            if (this.sorIndex(kar1) == this.sorIndex(kar2)) {
+                // egy sorban vannak
+
+                // TO BE CONTINUE
+
+                //int kar1_o = this.oszlopIndex(kar1);
+                //int kar2_o = this.oszlopIndex(kar2);
+                
+                //if (kar1_o != 4) {
+                    
+                //}
+            }
+            if (this.oszlopIndex(kar1) == this.oszlopIndex(kar2)) {
+            // egy oszlopban vannak
+            }
+
+            // a harmadik szabály érvényes
+
+
+            return "alma";
         }
     }
     class Program
@@ -40,6 +83,12 @@ namespace playfair_2
         {
             // 4. feladat
             PlayfairKodolo pk = new PlayfairKodolo("kulcstabla.txt");
+            // 6. feladat
+            Console.Write("6. feladat - Kérek egy nagybetűt: ");
+            char valasztott_betu = Console.ReadLine()[0];
+            Console.WriteLine("A karakter sorának az indexe: " + pk.sorIndex(valasztott_betu));
+            Console.WriteLine("A karakter oszlopának az indexe: " + pk.oszlopIndex(valasztott_betu));
+            Console.ReadKey();
         }
     }
 }
